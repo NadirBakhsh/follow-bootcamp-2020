9f3e8a67edaf7e8587ed475822cd3f061c4d34ac
Nadir Bakhsh <nadir4k2010@gmail.com>
2022-01-27 04:40:19 +0500

    fixes

--- expense-tracker/src/components/IncomeExpense/IncomeExpense.js
+++ expense-tracker/src/components/IncomeExpense/IncomeExpense.js
@@ -10,8 +10,6 @@ function IncomeExpense() {
 
   const amounts = transactions.map(transaction => transaction.amount);
 
-  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
-
   const income = amounts
     .filter(item => item > 0)
     .reduce((acc, item) => (acc += item), 0)
